47152079bd4de5faafe4afaffa4dd68e1c59f98d
zouzouek <zouzouek@gmail.com>
2016-04-25 11:47:14 +0300

    Fixed file directory

--- Eco-Printing/public_html/js/conf.js
+++
@@ -1,8 +0,0 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-var username = sessionStorage.getItem('userID');
-var ref = new Firebase("https://popping-fire-7207.firebaseio.com/users/" +username);
